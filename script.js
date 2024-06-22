4131ba4d2fc73f5b305b0c70e780024300cc8839
saloxiddin97 <salohiddinshamsidinov0101@gmail.com>
2024-06-22 10:04:00 +0500

    js10

---
+++ script.js
@@ -0,0 +1,29 @@
+//fuction decloration
+//olma()
+//function olma(){
+    //console.log("decloration");
+//}
+//fuction expretion
+
+//const myfunctin = function() {
+    //console.log(" expretion");
+//}
+//myfunctin()
+let a=prompt(" brinchi son kriting")
+let b=prompt(" ikinchi son kriting")
+
+
+if(a>b){
+    alert("birinchi  katta")
+    
+}
+else {
+    alert("ikinchi son katta")
+}
+
+
+
+
+
+    
+
